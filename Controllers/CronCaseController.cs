b9b73a7bed3a4fbfed7cf4fdd25ea8442f8c141f
Michael <michael-petersen@live.dk>
2024-06-11 14:01:50 +0200

    added popup box

--- Controllers/CronCaseController.cs
+++ Controllers/CronCaseController.cs
@@ -8,17 +8,26 @@ using summeringsmakker.Models;
 using summeringsMakker.Repository;
 using summeringsMakker.Services;
 
-
 namespace summeringsmakker.Controllers
 {
     [ApiController]
     [Route("cron")]
-    public class CronCaseController(
-        ICaseRepository caseRepository,
-        ICaseSummaryRepository caseSummaryRepository,
-        CaseProcessor caseProcessor)
-        : ControllerBase
+    public class CronCaseController : Controller
     {
+        private readonly ICaseRepository _caseRepository;
+        private readonly ICaseSummaryRepository _caseSummaryRepository;
+        private readonly CaseProcessor _caseProcessor;
+
+        public CronCaseController(
+            ICaseRepository caseRepository,
+            ICaseSummaryRepository caseSummaryRepository,
+            CaseProcessor caseProcessor)
+        {
+            _caseRepository = caseRepository;
+            _caseSummaryRepository = caseSummaryRepository;
+            _caseProcessor = caseProcessor;
+        }
+
         [HttpPost("create-case-summaries")]
         public async Task<IActionResult> CreateCaseSummaries()
         {
@@ -27,13 +36,13 @@ namespace summeringsmakker.Controllers
             DateTime endOfDay = startOfDay.AddDays(1).AddTicks(-1);
 
             // Fetch all cases for the current day from the data warehouse
-            var casesForPeriod = caseRepository.GetAll(startOfDay, endOfDay);
+            var casesForPeriod = _caseRepository.GetAll(startOfDay, endOfDay);
 
             // Extract the IDs of the fetched cases
             var caseIdsForPeriod = casesForPeriod.Select(c => c.Id).ToList();
 
             // Get the IDs of the case summaries that already exist
-            var existingCaseIds = caseSummaryRepository.GetCaseIds(caseIdsForPeriod);
+            var existingCaseIds = _caseSummaryRepository.GetCaseIds(caseIdsForPeriod);
 
             // Filter out the cases that already have summaries
             var casesWithoutSummaries = casesForPeriod
@@ -45,17 +54,21 @@ namespace summeringsmakker.Controllers
 
             foreach (var caseWithoutSummary in casesWithoutSummaries)
             {
-                
-                var caseSummary = await caseProcessor.ProcessFile(caseWithoutSummary);
+                var caseSummary = await _caseProcessor.ProcessFile(caseWithoutSummary);
                 caseSummaries.Add(caseSummary);
             }
 
             // add to database
-            caseSummaryRepository.Add(caseSummaries);
+            _caseSummaryRepository.Add(caseSummaries);
+
+            var message = casesWithoutSummaries.Count > 0
+                ? $"{casesWithoutSummaries.Count} case(s) were added"
+                : "Already up to date";
+
+            // Pass the message to the view
+            TempData["Notification"] = message;
 
             return RedirectToAction("Index", "CaseSummary");
         }
-
-
     }
 }
